a554a2c12cfe755b2f3a3bd13144dac8c4d09057
Tasha <129745242+TashaBolsh@users.noreply.github.com>
2023-04-21 11:57:37 +0300

    Напишите функцию
    Напишите функцию min(a,b), которая возвращает меньшее из чисел a и b.

---
+++ task3.js
@@ -0,0 +1,15 @@
+/* Напишите функцию min(a,b), которая возвращает меньшее из чисел a и b.
+
+Пример вызовов:   */
+
+function min(a,b){
+if(a < b) 
+minValue = a;
+else
+minValue = b
+return console.log(minValue);   
+}
+
+min(2, 5)
+min(3, -1)
+min(1, 1)
